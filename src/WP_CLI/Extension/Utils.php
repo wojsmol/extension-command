156267d22d159734af132f1d8b1c438bd71b1ded
Wojciech Smoliński <wojsmol@users.noreply.github.com>
2018-07-25 10:34:00 +0200

    try tocreate utils class

---
+++ src/WP_CLI/Extension/Utils.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace WP_CLI\Extension;
+
+class Utils {
+
+	/**
+	 * Gets the template path based on installation type.
+	 */
+
+	public static function get_template_path( $template ) {
+		$command_root = Utils\phar_safe_path( dirname( __DIR__ ) );
+		$template_path = "{$command_root}/templates/{$template}";
+
+		if ( ! file_exists( $template_path ) ) {
+			WP_CLI::error( "Couldn't find {$template}" );
+		}
+
+		return $template_path;
+	}
+}
